2d389430403725dba345fff4cc29891362cf0e2c
NelloRossi <rossianiello97@gmail.com>
2025-01-29 11:39:08 +0100

    fix attacks

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,23 +1,31 @@
+/* eslint-disable react/prop-types */
 import { useNavigate } from "react-router-dom";
 
-function Card({card}) {
+function Card({ card }) {
   const navigate = useNavigate();
   return (
     <div className="card card-side bg-base-200 shadow-xl w-96 mt-20 p-3">
       <figure className="w-full">
-        <img
-          src={card.images.large}
-          alt="Movie"
-        />
+        <img src={card.images.large} alt="Movie" />
       </figure>
       <div className="card-body">
         <p>{card.name}</p>
-        <p> {card.map((attacks) => (
-                    <p key={card.id} attack={attacks} />
-                ))} Attacco  </p>
-       <div className="card-actions justify-end">
+
+        {/* {card.attacks.map((attack, index) => (
+              <p key={index}>
+                Attacco {attack.name}
+              </p>
+          ))}*/}
+        {Array.isArray(card?.attacks) ? (
+          card.attacks.map((attack, index) => (
+            <p key={index}>Attacco {index + 1} : {attack.name}</p>
+          ))
+        ) : (
+          <p>Nessun attacco disponibile</p>
+        )}
+        <div className="card-actions justify-end">
           <button className="btn btn-primary" onClick={() => navigate("")}>
-            Compra 
+            Compra
           </button>
         </div>
       </div>
