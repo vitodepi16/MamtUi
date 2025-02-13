cba5f6706411d079876f8a989153f272b78b23b6
vitodepi16 <118888625+vitodepi16@users.noreply.github.com>
2025-02-13 12:24:17 +0100

    slider tiktok test

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import "../Home.css";
 import Header from "../components/Header.jsx";
 import Footer from "../components/Footer.jsx";
 import Hero from "../components/Hero.jsx";
+import Slider from "../components/Slider.jsx";
 
 function App() {
   return (
@@ -12,6 +13,9 @@ function App() {
       <main>
         <div className="flex flex-auto flex-wrap">
           <Hero></Hero>
+        </div>
+        <div>
+          <Slider />
         </div>
       </main>
       <Footer></Footer>
